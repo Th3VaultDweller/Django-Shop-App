ec798a028197830f3c6b5084e98a1e717eee7ce6
Eduard Aivazian <aztec242@yandex.ru>
2023-07-24 14:35:44 +0300

    Defined the views

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 # Create your views here.
+
+def product_list(request, category_slug=None):
+    """Репрезентация списка товаров"""
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all(available=True)
+    
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 
+                  'shop/products/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+def product_detail(request, id, slug):
+    """Репрезентация отображения одного товара"""
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+
+    return render(request, 
+                  'shop/products/detail.html', 
+                  {'product': product})
