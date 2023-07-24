a40f4c194943071c68c5bd205c9010dbd9419469
Eduard Aivazian <aztec242@yandex.ru>
2023-07-24 14:03:00 +0300

    Added some comments to the models

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -6,12 +6,12 @@ from .models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug':('name',)}
+    prepopulated_fields = {'slug':('name',)} # атрибут prepopulated_fields используется для того, чтобы указывать поля автоматически с использованием значения других полей
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 
                     'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
-    list_editable = ['price', 'available']
+    list_editable = ['price', 'available'] # атрибут list_editable используется для того, чтобы задать поля, которые модно редактировать, находясь на странице отображения списка на сайте администрирования
     prepopulated_fields = {'slug':('name',)}
