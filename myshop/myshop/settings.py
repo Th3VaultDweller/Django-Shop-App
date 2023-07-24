493b314b6874dba0ba76b2ac5fd50d6545088713
Eduard Aivazian <aztec242@yandex.ru>
2023-07-24 15:49:13 +0300

    Added MEDIA_URL and MEDIA_ROOT

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/' # MEDIA_URL  – это базовый URL-адрес, по которому раздаются закачанные пользователями медиафайлы
+MEDIA_ROOT = BASE_DIR / 'media' # MEDIA_ROOT – это локальный путь, по которому эти файлы находятся и который следует формировать динамически, добавляя значение переменной BASE_DIR в качестве префикса
