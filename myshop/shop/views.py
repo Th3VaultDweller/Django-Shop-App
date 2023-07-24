3749f56d5a6e0de21fee7161cef1732828001b05
Eduard Aivazian <aztec242@yandex.ru>
2023-07-24 16:00:40 +0300

    Minor fixes

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -7,13 +7,13 @@ def product_list(request, category_slug=None):
     """Репрезентация списка товаров"""
     category = None
     categories = Category.objects.all()
-    products = Product.objects.all(available=True)
+    products = Product.objects.filter(available=True)
     
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request, 
-                  'shop/products/list.html',
+                  'shop/product/list.html',
                   {'category': category,
                    'categories': categories,
                    'products': products})
@@ -23,5 +23,5 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
 
     return render(request, 
-                  'shop/products/detail.html', 
-                  {'product': product})
+                  'shop/product/detail.html', 
+                  {'product': product}) 
