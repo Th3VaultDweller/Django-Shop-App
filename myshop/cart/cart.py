7f4e34ec533e905eaa9ff671ebf9a31ef1608524
Eduard Aivazian <aztec242@yandex.ru>
2023-07-27 18:50:37 +0300

    Defined base functionality of the Cart

---
+++ myshop/cart/cart.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+from django.conf import settings
+from shop.models import Product
+
+class Cart:
+    def __init__(self, request):
+        """Инициализация корзины"""
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # сохранить пустую корзину в сеансе
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+        def add(self, quantity=1, override_quantity=False):
+            """Добавить товар в корзину, либо обновить его количество"""
+            product_id = str(product.id)
+            if product_id not in self.cart:
+                self.cart[product_id] = {'quantity': 0,
+                                         'price': str(product.price)
+                                         }
+            if override_quantity:
+                self.cart[product_id]['quantity'] = quantity
+            else:
+                self.cart[product_id]['quantity'] += quantity
+            self.save()
+
+        def save(self):
+            """Пометить сеанс как изменённый, чтобы обеспечить его сохранение"""
+            self.session.modified = True
