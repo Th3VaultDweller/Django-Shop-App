1e2c047ecbcb8f540409b36c5ed1026fe844a249
Eduard Aivazian <aztec242@yandex.ru>
2023-07-27 19:09:24 +0300

    Fixes to the Add function

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -12,7 +12,7 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-    def add(self, quantity=1, override_quantity=False):
+    def add(self, product, quantity=1, override_quantity=False):
         """Добавить товар в корзину, либо обновить его количество"""
         product_id = str(product.id)
         if product_id not in self.cart:
