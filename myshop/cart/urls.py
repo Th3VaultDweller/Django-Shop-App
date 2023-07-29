f642b81719092be683f0ba94e119901b05e070c2
Eduard Aivazian <aztec242@yandex.ru>
2023-07-29 16:47:55 +0300

    Minor fixes

--- myshop/cart/urls.py
+++ myshop/cart/urls.py
@@ -6,7 +6,7 @@ app_name = 'cart'
 urlpatterns = [
     path('', views.cart_detail, 
          name='cart_detail'),
-    path('add/<int:pruduct_id>', views.cart_add, 
+    path('add/<int:product_id>', views.cart_add, 
          name='cart_add'),
     path('remove/<int:product_id>', views.cart_remove, 
          name='cart_remove'),
