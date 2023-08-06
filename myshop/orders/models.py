f9721dce17307bdf9dbe17c411f37a2dee775458
Eduard Aivazian <aztec242@yandex.ru>
2023-08-06 18:52:02 +0300

    Typo fixes

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -8,7 +8,7 @@ class Order(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
-    address = models.CharField(max=250)
+    address = models.CharField(max_length=250)
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=250)
     created = models.DateField(auto_now_add=True) # # auto_now_add добавляет дату и время только на момент создания товара
