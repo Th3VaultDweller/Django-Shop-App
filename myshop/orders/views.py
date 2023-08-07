5a26aabecbac5c972fb31a540a50ea7a353acc0a
Eduard Aivazian <aztec242@yandex.ru>
2023-08-07 16:59:24 +0300

    Created a view ffor order creation

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
 
 # Create your views here.
+
+def order_create(request):
+    """Создание заказа"""
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid:
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         products=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+                
+                cart.clear() # очистка корзины
+                return render(request,'orders/order/created.html',
+                              {'order': order})
+        else:
+            form=OrderCreateForm()
+        
+        return render(request,'orders/order/created.html',
+                      {'cart': cart, 'form': form})
+
