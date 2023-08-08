22c07b3355f66356a54a5cd604d453b5bd016da5
Eduard Aivazian <aztec242@yandex.ru>
2023-08-08 13:54:30 +0300

    Added a comment to the Meta class

--- myshop/orders/forms.py
+++ myshop/orders/forms.py
@@ -3,7 +3,8 @@ from .models import Order
 
 class OrderCreateForm(forms.ModelForm):
     """Форма создания заказа"""
-    class Meta:
+    # внутренний класс класса модели. Метамодель в основном используется для изменения поведения полей модели, таких как изменение опций заказа, verbose_name, и многих других параметров.
+    class Meta: 
         model = Order
         fields = ['first_name',
                 'last_name',
