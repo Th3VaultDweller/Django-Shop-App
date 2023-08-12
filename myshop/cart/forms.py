a07b45e11663b13c4972850c6192af671c7a55e6
Eduard Aivazian <aztec242@yandex.ru>
2023-08-12 19:38:39 +0300

    Added a label

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.utils.translation import gettext_lazy as _ # перевод
 
 # выбор от 1 до 20
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
@@ -7,7 +7,8 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 class CartAddProductForm(forms.Form):
     """Форма для добавления товара в корзину"""
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
-                                      coerce=int)
+                                      coerce=int,
+                                      label = _('Quantity')) # метка
     override = forms.BooleanField(required=False,
                                   initial=False,
                                   widget=forms.HiddenInput)
