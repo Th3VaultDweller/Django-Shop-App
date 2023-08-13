69814606a3bde09205f9fc0d7758ae5447971e0c
Eduard Aivazian <aztec242@yandex.ru>
2023-08-13 20:33:51 +0300

    Included Rosetta

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('rosetta/', include('rosetta.urls')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
