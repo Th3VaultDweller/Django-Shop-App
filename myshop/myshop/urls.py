c7cb45397a3bd440e5c66fa4e09e14a497ecad0b
Eduard Aivazian <aztec242@yandex.ru>
2023-08-16 13:02:43 +0300

    Added lazy translation

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -19,11 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders')),
+    path(_('cart/'), include('cart.urls', namespace='cart')),
+    path(_('orders/'), include('orders.urls', namespace='orders')),
     path('rosetta/', include('rosetta.urls')),
     path('', include('shop.urls', namespace='shop')),
 )
