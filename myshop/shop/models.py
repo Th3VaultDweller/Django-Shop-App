cdf0fd248f512cd62fa6ca9368f38009354df097
Eduard Aivazian <aztec242@yandex.ru>
2023-08-19 19:39:25 +0300

    Updated model fields to be translatable

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.urls import reverse
+from parler.models import TranslatableModel, TranslatedFields
 
-class Category(models.Model):
+class Category(TranslatableModel):
     """Репрезентация категории товара в интернет-магазине"""
-    name = models.CharField(max_length=255) # название товара
-    slug = models.SlugField(max_length=255, unique=True) # слаг товара для красивого URL-адреса
+    # переводные поля модели, сгенерированные Parler'ом
+    translations = TranslatedFields(
+        name = models.CharField(max_length=255), # название товара
+        slug = models.SlugField(max_length=255, unique=True) # слаг товара для красивого URL-адреса
+    )
 
     # внутренний класс класса модели. Метамодель в основном используется для изменения поведения полей модели, таких как изменение опций заказа, verbose_name, и многих других параметров.
     class Meta:
-        ordering = ['name']
-        indexes = [models.Index(fields=['name']),]
+        # ordering = ['name']
+        # indexes = [models.Index(fields=['name']),]
         verbose_name = 'category'
         verbose_name_plural = 'categories'
     
@@ -20,15 +24,18 @@ class Category(models.Model):
         return reverse('shop:product_list_by_category',
                        args=[self.slug])
 
-class Product(models.Model):
+class Product(TranslatableModel):
     """Репрезентация товара в интернет-магазине"""
+    # переводные поля модели, сгенерированные Parler'ом
+    translations = TranslatedFields(
+        name = models.CharField(max_length=255),
+        slug = models.SlugField(max_length=255),
+        description = models.TextField(blank=True),
+    )
     category = models.ForeignKey(Category,
                                  related_name='products',
                                  on_delete=models.CASCADE) # CASCADE автоматически удаляет строку из зависимой таблицы, если удаляется связанная строка из главной таблицы
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2) # для хранения значений денежных сумм всегда следует использовать DecimalField. Используя Decimal удастся избежать проблем с плавающей запятой
     available = models.BooleanField(blank=True) # Булево значение (да или нет)
     created = models.DateTimeField(auto_now_add=True) # auto_now_add добавляет дату и время только на момент создания товара
