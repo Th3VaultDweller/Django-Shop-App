dcdd8f2e0ab745c45e98aaaa83ac1bf1ad60e4ec
Eduard Aivazian <aztec242@yandex.ru>
2023-08-20 22:22:21 +0300

    Added localflavor postal code

--- myshop/orders/forms.py
+++ myshop/orders/forms.py
@@ -1,10 +1,12 @@
 from django import forms
+from localflavor.us.forms import USZipCodeField
 from .models import Order
 
 class OrderCreateForm(forms.ModelForm):
     """Форма создания заказа"""
     # внутренний класс класса модели. Метамодель в основном используется для изменения поведения полей модели, таких как изменение опций заказа, verbose_name, и многих других параметров.
     class Meta: 
+        postal_code = USZipCodeField # почтовый индекс в США
         model = Order
         fields = ['first_name',
                 'last_name',
